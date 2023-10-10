176f5b50ef8ae8bc5a37345d4b01949e83228f9e
SharuSiv <122399531+SharuSiv@users.noreply.github.com>
2023-10-10 11:54:48 -0400

    main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ browser.implicitly_wait(5)
 
 #driver navigates to instagram main page
 browser.get('https://www.instagram.com/')
-def login():
+def login(username,password):
     #inserts the username and password, and logs in to instagram
-    browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input").send_keys("// PLACE THE USERNAME OF YOUR ACCOUNT HERE")
-    browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input").send_keys("// PLACE THE PASSWORD OF YOUR ACCOUNT HERE")
+    browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input").send_keys(username)
+    browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input").send_keys(password)
     browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button").click()
     #clears the pop ups after login
     time.sleep(3)
@@ -36,17 +36,13 @@ def friendreq(user):
     browser.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/div[2]/section/main/div/header/section/div[1]/div[1]/div/div/button").click()
 
 def message_reply():
-    #User can insert as many automated messages as they want, the bot will randomaly select and reply with on
-    messages = ["INSERT MESSAGES HERE"]
+    messages = ["PLACE MESSAGES HERE"]
     message_box = browser.find_element(By.XPATH, "//div[@aria-label='Message']")
     random_word = random.choice(messages)
-    #breaks down the message into chars and sends each char into message box
     for char in random_word:
         message_box.send_keys(char)
         time.sleep(0.1)
-    # sends the message to user's dms
     browser.find_element(By.XPATH, "//div[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1i0vuye x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37 xfs2ol5' and @role='button' and @tabindex='0']").click()
-    # redirects the browser to main messaging page so that driver isn't confused (if they stay on one dm and that user messages, the driver wont be able to reply because it doesn't identify that dm as a new message)
     browser.find_element(By.XPATH, "//a[@aria-label='Direct messaging - 0 new notifications link']").click()
 # Function to continuously monitor incoming messages and respond
 def monitor_messages():
@@ -55,7 +51,6 @@ def monitor_messages():
     browser.find_element(By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/div/div/span/div/a/div").click()
 
     while retry_count < max_retries:
-        #exception handling, this allows the code to keep running and even if no one messages
         try:
             # Wait for the message input field to load
             WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[1]")))
@@ -71,17 +66,18 @@ def monitor_messages():
                 # Perform actions on the button
                 button_element.click()
                 message_reply()
+                # Wait for a while before checking again (adjust the sleep duration as needed)
 
         except Exception as e:
             print(f"An error occurred: {str(e)}")
-            # if the driver cant find new messages after 10 iterations, the while loop breaks
             retry_count+=1
             time.sleep(5)  # Wait before retrying
 
 
 #main driver code here
-login()
-# Allows the user to friend request whoever they wish to follow, after user inputs N the driver then starts the auto messaging process
+username = input("type in the username of your bot: ")
+password = input("type in the password of the bot: ")
+login(username,password)
 while True:
     user_input = input("Do you want to send a friend request (Y/N)? ").strip().upper()
     if user_input == 'N':
@@ -95,5 +91,6 @@ while True:
     friendreq(username)
 
 monitor_messages()
+
 browser.close()
 
